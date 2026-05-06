18aa3324c776d3c9b718df5caae57d866acdfb30
qoun2004 <qoun2004@gmail.com>
2026-05-06 15:58:17 +0800

    Add files via upload

---
+++ conga-sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'conga-v1';
+const ASSETS = ['/recrete.html', '/conga-manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
